84f5f8cb40a8c1fcd1eed18f03ceaa5db2b1073a
mkspcd <mkspcd@gmail.com>
2018-03-26 00:14:34 +0200

    Chapter 2 : Java statements.

--- src/com/sybex/chapter2/Statements.java
+++ src/com/sybex/chapter2/Statements.java
@@ -5,23 +5,50 @@ public class Statements {
   public static void main(String[] args) {
 
     boolean b = true;
-    int x = 9;
+    int x = 0;
+    final int y = 3;
     
-    x = b ? increase(x) : decrease(x);
+    String s = "foo";
+    
+//    x = b ? increase(x) : decrease(x);
 //    x = b ? 1 : 2;
-//    b ? print("The true") : print("The false");
+//    b ? print("The true") : print("The false"); // Does not compile.
+    
+    System.out.println("b : " + b);
 
     // Switch
     System.out.println("x is : " + x);
     switch (x) {
       case 0 :
         System.out.println("Zero");
+//        break;
       case 1 :
         System.out.println("Uno");
+//        break;
       case 2 :
         System.out.println("Two");
+//        break;
+      case y :
+        System.out.println("Two");
+//        break;
       default :
         System.out.println("Default case.");
+//        break;
+    }
+    
+    switch(s) {
+      case "test" :
+        System.out.println("test");
+        break;
+      case "foo" :
+        System.out.println("foo");
+        break;
+      case "bar" :
+        System.out.println("bar");
+        break;
+      default :
+        System.out.println("default");
+        break;
     }
     
   }
@@ -37,4 +64,5 @@ public class Statements {
   public static void print(String s) {
     System.out.println(s);
   }
+
 }
