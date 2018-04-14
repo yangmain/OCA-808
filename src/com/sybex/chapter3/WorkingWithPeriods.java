23ef2538600de149bef259c1c1a8b5c765df55a2
mkspcd <mkspcd@gmail.com>
2018-04-14 14:14:37 +0200

    Complete working w/ periods

--- src/com/sybex/chapter3/WorkingWithPeriods.java
+++ src/com/sybex/chapter3/WorkingWithPeriods.java
@@ -5,8 +5,24 @@ import java.time.*;
 public class WorkingWithPeriods {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
 
+    LocalDate date1 = LocalDate.of(2018, 04, 14);
+    System.out.println("date 1 : " + date1);
+
+    LocalDate date2 = LocalDate.of(2018, Month.APRIL, 14);
+    System.out.println("date 2 : " + date2);
+
+    LocalDate date3 = LocalDate.of(2018, Month.JUNE, 1);
+    System.out.println("date 3 : " + date3);
+    
+    System.out.println(date1.isEqual(date2));
+    System.out.println(date1.isBefore(date3));
+    System.out.println(date1.isAfter(date3));
+    
+    Period pAnnual = Period.ofYears(1);
+    
+    date1 = date1.plus(pAnnual);
+    System.out.println("date 1 : " + date1);
   }
 
 }
