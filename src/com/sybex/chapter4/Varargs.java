6d8125c16976d9dc4e86f37d99371ef1f918a1d0
mkspcd <mkspcd@gmail.com>
2018-04-14 19:47:55 +0200

    Continue varargs

--- src/com/sybex/chapter4/Varargs.java
+++ src/com/sybex/chapter4/Varargs.java
@@ -3,12 +3,24 @@ package com.sybex.chapter4;
 public class Varargs {
 
   public static void main(String[] args) {
+    walk(1);
+    walk(1, 2);
+    walk(1, 2, 3, 4);
+    walk(1, new int[] {2, 3, 4});
 
-
+    run(1, null);
   }
-
   
   public static void walk(int start, int... nums) {
     System.out.println(nums.length);
   }
+  
+  public static void run(int start, int... nums) {
+    if(nums != null) {
+      System.out.println(nums.length);
+    } else {
+      System.out.println("null");
+    }
+  }
+  
 }
