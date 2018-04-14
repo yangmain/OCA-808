d4f579bca5fa306149f69e5c47cd13f2cf2b341c
mkspcd <mkspcd@gmail.com>
2018-04-14 13:20:31 +0200

    Add conversion between Array and List

---
+++ src/com/sybex/chapter3/ConvertingBetweenArrayAndList.java
@@ -0,0 +1,38 @@
+package com.sybex.chapter3;
+
+import java.util.*;
+
+public class ConvertingBetweenArrayAndList {
+
+  public static void main(String[] args) {
+    // List to array
+    List<String> list = new ArrayList<>();
+    list.add("Mario");
+    list.add("Luigi");
+    System.out.println(list);
+
+    Object[] objectArray = list.toArray();
+    System.out.println(objectArray);
+    
+    String[] stringArray = list.toArray(new String[10]);
+    System.out.println(stringArray.length);
+
+    // Array to list
+    String [] array = {"Wario", "Toad", "Yoshi"};
+    System.out.println(array);
+    
+    List<String> stringList = Arrays.asList(array);
+    System.out.println(stringList);
+    System.out.println(stringList.size());
+    
+    stringList.set(1, "Peach");
+    for(String s : array) System.out.print(s + " ");
+    System.out.println(stringList);
+    System.out.println(stringList.size());
+    
+    array[0] = "Mario";
+    for(String s : stringList) System.out.print(s + " ");
+    
+  }
+
+}
