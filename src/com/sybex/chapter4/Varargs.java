768c990778701ea9cbc8331c540aa5a3c286f971
mkspcd <mkspcd@gmail.com>
2018-04-14 19:47:39 +0200

    Add varargs

---
+++ src/com/sybex/chapter4/Varargs.java
@@ -0,0 +1,14 @@
+package com.sybex.chapter4;
+
+public class Varargs {
+
+  public static void main(String[] args) {
+
+
+  }
+
+  
+  public static void walk(int start, int... nums) {
+    System.out.println(nums.length);
+  }
+}
