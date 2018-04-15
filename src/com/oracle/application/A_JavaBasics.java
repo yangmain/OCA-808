0963868fe9bb6741fa8472cfd832bc041c67b04a
mkspcd <mkspcd@gmail.com>
2018-04-15 22:47:22 +0200

    Add questions Java basics

---
+++ src/com/oracle/application/A_JavaBasics.java
@@ -0,0 +1,12 @@
+package com.oracle.application;
+
+import java.*;
+
+public class A_JavaBasics {
+
+  public static void main(String[] args) {
+
+    float density = (float) 1.0;
+  }
+
+}
--- src/com/sybex/chapter4/Lambdas.java
+++ src/com/sybex/chapter4/Lambdas.java
@@ -3,8 +3,8 @@ package com.sybex.chapter4;
 public class Lambdas {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
 
+    
   }
 
 }
