824166741a3576c63932d4ffccdd8d6c7f993a79
mkspcd <mkspcd@gmail.com>
2018-04-16 06:09:21 +0200

    Add working with data types

---
+++ src/com/oracle/application/B_WorkingWithDataTypes.java
@@ -0,0 +1,21 @@
+package com.oracle.application;
+
+public class B_WorkingWithDataTypes {
+
+  public static void main(String[] args) {
+    int i = 3;
+    long l = 4;
+    
+    long l2 = 4L;
+    
+    System.out.println(i);
+    System.out.println(l);
+    
+    double d = 1.3;
+    float f = 1.2F;
+    
+    System.out.println(d);
+    System.out.println(f);
+  }
+
+}
