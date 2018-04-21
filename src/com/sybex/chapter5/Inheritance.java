663688f7460af50923dca43947e878db8aeefe61
mkspcd <mkspcd@gmail.com>
2018-04-21 19:19:30 +0200

    Add hiding methods vs override

--- src/com/sybex/chapter5/Inheritance.java
+++ src/com/sybex/chapter5/Inheritance.java
@@ -3,8 +3,53 @@ package com.sybex.chapter5;
 public class Inheritance {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-
+    Chicken c = new Chicken("Blue");    
+//    System.out.println(c.color);
   }
 
+}
+
+class Bird {
+  
+  protected String color;
+  
+  Bird() {
+    System.out.println("Hello Bird!");
+  }
+  
+  Bird(String color) {
+    System.out.println("Bird color.");
+  }
+
+  public void printName() {
+    System.out.println("Bird name :)");
+  }
+  
+}
+
+class Chicken extends Bird {
+  
+  public Chicken() {
+    super();
+    System.out.println("Hello Chicken!");
+    color = "red";
+  }
+  
+  public Chicken(String color) {
+    this();
+    this.color = color;
+    super.printName();
+  }
+  
+  // Override
+  public void printName() {
+    System.out.println("Chicken name :)");
+  }
+  
+  // Overload
+  public int printName(int value) {
+    System.out.println("Chicken name :)");
+    return 0;
+  }
+  
 }
---
+++ src/com/sybex/chapter5/Kangaroo.java
@@ -0,0 +1,39 @@
+package com.sybex.chapter5;
+
+/**
+ * Hiding methods.
+ */
+public class Kangaroo extends Marsupial {
+
+  public static boolean isBiped() {
+    return true;
+  }
+  
+  public void getKangarooDescription() {
+    System.out.println("Kangaroo hops on two legs : " + isBiped());
+  }
+  
+  public static void main(String[] args) {
+    System.out.println("KANGAROO : ");
+    Kangaroo k = new Kangaroo();
+    k.getMarsupialDescription();
+    k.getKangarooDescription();
+
+    System.out.println("\nMARSUPIAL : ");
+    Marsupial m = new Marsupial();
+    m.getMarsupialDescription();
+  }
+
+}
+
+class Marsupial {
+  
+  public static boolean isBiped() {
+    return false;
+  }
+  
+  public void getMarsupialDescription() {
+    System.out.println("Marsupial walks on two legs : " + isBiped());
+  }
+  
+}
